b4bed121fa93ab424e76fae2e4f642bcd65372c3
lchzh3473 <lchzh3473@163.com>
2023-04-14 10:33:18 +0800

    firefox supports

--- public/utils/main.js
+++ public/utils/main.js
@@ -196,20 +196,24 @@ const Utils = {
 	Utils.loadJS(`
 		var _hmt = _hmt || [];
 		(function() {
-		var hm = document.createElement("script");
-		hm.src = "https://hm.baidu.com/hm.js?a2847526e110755a92e0ed1c8e948a32";
-		var s = document.getElementsByTagName("script")[0]; 
-		s.parentNode.insertBefore(hm, s);
+			var hm = document.createElement("script");
+			hm.src = "https://hm.baidu.com/hm.js?a2847526e110755a92e0ed1c8e948a32";
+			var s = document.getElementsByTagName("script")[0]; 
+			s.parentNode.insertBefore(hm, s);
 		})();
 	`);
 	/* Global site tag (gtag.js) - Google Analytics */
-	Utils.loadJS('https://www.googletagmanager.com/gtag/js?id=G-K98WR056RJ').then(
-		() => Utils.loadJS(`
+	Utils.loadJS(`
 		window.dataLayer = window.dataLayer || [];
 		function gtag(){dataLayer.push(arguments);}
 		gtag('js', new Date());
 		gtag('config', 'G-K98WR056RJ');
-	`));
+		window.addEventListener('load', function(){
+			var s = document.createElement('script');
+			s.src = 'https://www.googletagmanager.com/gtag/js?id=G-K98WR056RJ';
+			document.body.appendChild(s);
+		});
+	`);
 	/* sdk.51.la */
 	Utils.loadJS('!function(p){"use strict";!function(t){var s=window,e=document,i=p,c="".concat("https:"===e.location.protocol?"https://":"http://","sdk.51.la/js-sdk-pro.min.js"),n=e.createElement("script"),r=e.getElementsByTagName("script")[0];n.type="text/javascript",n.setAttribute("charset","UTF-8"),n.async=!0,n.src=c,n.id="LA_COLLECT",i.d=n;var o=function(){s.LA.ids.push(i)};s.LA?s.LA.ids&&o():(s.LA=p,s.LA.ids=[],o()),r.parentNode.insertBefore(n,r)}()}({id:"Jj67QM9XKxdyBUKP",ck:"Jj67QM9XKxdyBUKP"});');
 	Utils.loadJS('https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8369333813205733');
@@ -223,8 +227,9 @@ Utils.lazyload(function() {
 	const s = new URLSearchParams(location.search);
 	if (!location.port && !s.has('test')) setInterval(Function.constructor(atob('ZGVidWdnZXI7')));
 	if (typeof _i == 'undefined' || _i.length != 4) return;
+	const hasReferrer = !!document.referrer || self.history.length > 1;
 	const isStandAlone = navigator.standalone || self.matchMedia('(display-mode: standalone)').matches;
-	if (jct && jct[1] == 'ok' || document.referrer || isStandAlone) document.cookie = `jct=ok;path=/;max-age=${2e6}`;
+	if (jct && jct[1] == 'ok' || hasReferrer || isStandAlone) document.cookie = `jct=ok;path=/;max-age=${2e6}`;
 	else if (!location.port && !Utils.checkED(s.get('ss'))) return location.href = '/401.html';
 	document.title = `${_i[0]} - ${d}制作`;
 	for (const i of document.querySelectorAll('.title')) i.innerHTML = `${_i[0]}&nbsp;<small>v${_i[1].join('.')}</small>`;
