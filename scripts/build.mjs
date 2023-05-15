2406606e8a43395113ab63b37a40663d4652bafc
lchzh3473 <lchzh3473@163.com>
2023-05-15 13:04:30 +0800

    add deploy

---
+++ scripts/build.mjs
@@ -0,0 +1,9 @@
+import fs from 'fs';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+const dir = fileURLToPath(new URL('.', import.meta.url));
+const source = resolve(dir, '../public');
+const target = resolve(dir, '../dist');
+// fs.rmSync(target, { recursive: true, force: true });
+// fs.mkdirSync(target);
+fs.cpSync('public/', 'dist/', { recursive: true });
